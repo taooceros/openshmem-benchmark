eedf26b60963b728ab8dfefc01f16a2536147193
Hongtao <hongtao_zhang@outlook.com>
2025-04-02 02:57:16 +0000

    remove bound check

--- src/bin/benchmark/main.rs
+++ src/bin/benchmark/main.rs
@@ -259,16 +259,20 @@ fn benchmark_loop<'a>(
 
             // let now = std::time::Instant::now();
 
-            // if my_pe >= num_concurrency {
-            //     let check_epoch = seed % epoch_size;
-            //     let check_data = seed % data_size;
-            //     if source[check_epoch][check_data] != dest[check_epoch][check_data] {
-            //         println!(
-            //             "pe {my_pe} epoch {epoch} check failed: {:?} != {:?}",
-            //             source[check_epoch], dest[check_epoch]
-            //         );
-            //     }
-            // }
+            if my_pe >= num_concurrency {
+                let check_epoch = seed % epoch_size;
+                let check_data = seed % data_size;
+                unsafe {
+                    if source.get_unchecked(check_epoch).get_unchecked(check_data)
+                        != dest.get_unchecked(check_epoch).get_unchecked(check_data)
+                    {
+                        println!(
+                            "pe {my_pe} epoch {epoch} check failed: {:?} != {:?}",
+                            source[check_epoch], dest[check_epoch]
+                        );
+                    }
+                }
+            }
 
             // println!("elapsed time: {}", now.elapsed().as_micros());
         }
